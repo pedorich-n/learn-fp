c27f56de336c465cf9371fa61f3e93e15ab51c14
Nikita Pedorich <pedorich.n@gmail.com>
2018-10-30 18:10:30 +0200

    Comonad style fixed

--- src/main/scala/learnfp/comonad/CoMonad.scala
+++ src/main/scala/learnfp/comonad/CoMonad.scala
@@ -1,15 +1,15 @@
 package learnfp.comonad
 
 trait CoMonad[F[_]] {
-  def counit[A](a:F[A]):A
-  def coFlatMap[A, B](a:F[A])(fx:F[A] => B):F[B]
+  def counit[A](a: F[A]): A
+  def coFlatMap[A, B](a: F[A])(fx: F[A] => B): F[B]
 }
 
-class CoMonadOps[F[_], A](a:F[A])(implicit coMonad: CoMonad[F]) {
-  def counit = coMonad.counit(a)
-  def coFlatMap[B](fx:F[A] => B):F[B] = coMonad.coFlatMap(a)(fx)
+class CoMonadOps[F[_], A](a: F[A])(implicit coMonad: CoMonad[F]) {
+  def counit: A = coMonad.counit(a)
+  def coFlatMap[B](fx: F[A] => B): F[B] = coMonad.coFlatMap(a)(fx)
 }
 
 object CoMonadOps {
-  implicit def toCoMonadOps[F[_], A](a:F[A])(implicit coMonad: CoMonad[F]) = new CoMonadOps[F, A](a)
+  implicit def toCoMonadOps[F[_], A](a: F[A])(implicit coMonad: CoMonad[F]): CoMonadOps[F, A] = new CoMonadOps[F, A](a)
 }
--- src/main/scala/learnfp/comonad/Id.scala
+++ src/main/scala/learnfp/comonad/Id.scala
@@ -4,10 +4,8 @@ import learnfp.functor.Id
 import learnfp.functor.IdInstance._
 
 object IdInstance {
-  implicit val idCoMonadInstance = new CoMonad[Id] {
+  implicit val idCoMonadInstance: CoMonad[Id] = new CoMonad[Id] {
     override def counit[A](a: Id[A]): A = ???
     override def coFlatMap[A, B](a: Id[A])(fx: Id[A] => B): Id[B] = ???
   }
 }
-
-
