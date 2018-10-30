ebaf32685dd7e98e8e6673e3efdb93f449aff743
Nikita Pedorich <pedorich.n@gmail.com>
2018-10-30 18:14:55 +0200

    Free style fixed

--- src/main/scala/learnfp/free/Free.scala
+++ src/main/scala/learnfp/free/Free.scala
@@ -6,29 +6,31 @@ import learnfp.functor.{Functor, FunctorOps}
 import scala.annotation.tailrec
 
 sealed trait Free[F[_], A]
-final case class Return[F[_], A](a:A) extends Free[F, A]
-final case class FlatMap[F[_], A, B](a:Free[F, A], fx:A => Free[F, B]) extends Free[F, B]
-final case class LiftF[F[_], A](fn:F[A]) extends Free[F, A]
+final case class Return[F[_], A](a: A) extends Free[F, A]
+final case class FlatMap[F[_], A, B](a: Free[F, A], fx: A => Free[F, B]) extends Free[F, B]
+final case class LiftF[F[_], A](fn: F[A]) extends Free[F, A]
 
 abstract class Natural[F[_], G[_]] {
-  def transform[A](a:F[A]):G[A]
+  def transform[A](a: F[A]): G[A]
 }
 
 object Free {
-  implicit def freeFunctorInstance[F[_]] = new Functor[({type E[X] = Free[F, X]})#E] {
+  implicit def freeFunctorInstance[F[_]]: Functor[({ type E[X] = Free[F, X] })#E] = new Functor[({ type E[X] = Free[F, X] })#E] {
     override def fmap[A, B](a: Free[F, A])(fx: A => B): Free[F, B] = ???
   }
 
-  implicit def freeToFunctorOps[F[_], A](a:Free[F, A]) = new FunctorOps[A, ({type E[X] = Free[F, X]})#E](a)
+  implicit def freeToFunctorOps[F[_], A](a: Free[F, A]): FunctorOps[A, ({ type E[X] = Free[F, X] })#E] =
+    new FunctorOps[A, ({ type E[X] = Free[F, X] })#E](a)
 
-  implicit def freeMonadInstance[F[_]] = new Monad[({type E[X] = Free[F, X]})#E] {
+  implicit def freeMonadInstance[F[_]]: Monad[({ type E[X] = Free[F, X] })#E] = new Monad[({ type E[X] = Free[F, X] })#E] {
     override def pure[A](a: A): Free[F, A] = ???
     override def flatMap[A, B](a: Free[F, A])(fx: A => Free[F, B]): Free[F, B] = ???
   }
 
-  implicit def freeToMonadOps[F[_], A](a:Free[F, A]) = new MonadOps[A, ({type E[X] = Free[F, X]})#E](a)
+  implicit def freeToMonadOps[F[_], A](a: Free[F, A]): MonadOps[A, ({ type E[X] = Free[F, X] })#E] =
+    new MonadOps[A, ({ type E[X] = Free[F, X] })#E](a)
 
-  def liftF[F[_], A](a:F[A]):Free[F, A] = LiftF[F, A](a)
+  def liftF[F[_], A](a: F[A]): Free[F, A] = LiftF[F, A](a)
 
-  def foldF[F[_], M[_], A](a:Free[F, A])(trans:Natural[F, M])(implicit f:Functor[M], m:Monad[M]):M[A] = ???
+  def foldF[F[_], M[_], A](a: Free[F, A])(trans: Natural[F, M])(implicit f: Functor[M], m: Monad[M]): M[A] = ???
 }
