e3aa2068846a6cf828ecce94417b58003f66522b
Nikita Pedorich <pedorich.n@gmail.com>
2018-10-30 18:17:39 +0200

    IO style fixed

--- src/main/scala/learnfp/io/IO.scala
+++ src/main/scala/learnfp/io/IO.scala
@@ -1,25 +1,31 @@
 package learnfp.io
 
-import learnfp.functor.Functor
 import learnfp.applicative.Applicative
+import learnfp.functor.Functor
 import learnfp.monad.Monad
 
-case class IO[R](run:() => R)
+case class IO[R](run: () => R)
 
 object IO {
-  implicit val ioFunctorInstance = new Functor[IO] {
-    override def fmap[A, B](a: IO[A])(fx: A => B): IO[B] = IO { () => fx(a.run()) }
+  implicit val ioFunctorInstance: Functor[IO] = new Functor[IO] {
+    override def fmap[A, B](a: IO[A])(fx: A => B): IO[B] = IO { () =>
+      fx(a.run())
+    }
   }
 
-  implicit val ioApplicativeInstance = new Applicative[IO] {
-    override def pure[A](a: A): IO[A] = IO { () => a }
-    override def <*>[A, R](fx: IO[A => R])(a: IO[A]): IO[R] = IO {() => fx.run()(a.run())}
+  implicit val ioApplicativeInstance: Applicative[IO] = new Applicative[IO] {
+    override def pure[A](a: A): IO[A] = IO { () =>
+      a
+    }
+    override def <*>[A, R](fx: IO[A => R])(a: IO[A]): IO[R] = IO { () =>
+      fx.run()(a.run())
+    }
   }
 
-  implicit val ioMonadInstance = new Monad[IO] {
+  implicit val ioMonadInstance: Monad[IO] = new Monad[IO] {
     override def pure[A](a: A): IO[A] = ioApplicativeInstance.pure(a)
     override def flatMap[A, B](a: IO[A])(fx: A => IO[B]): IO[B] = fx(a.run())
   }
 
-  def runIO[R](io:IO[R]):R = io.run()
+  def runIO[R](io: IO[R]): R = io.run()
 }
